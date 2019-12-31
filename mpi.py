461d66bdead8447fc724b5abd454a98308d20400
Taha Korkmaz <33196763+tahakorkmaz14@users.noreply.github.com>
2019-12-31 13:48:44 +0300

    Update mpi.py
    BugFix of odd ones not taking the correct rightTop, Left and leftBot. Works like a charm now.

--- mpi.py
+++ mpi.py
@@ -11,9 +11,9 @@ import time
 #Working status: Working
 #Notes: User can set the input,output and iteration numbers below
 
-INPUT_FILE="test.txt" #assign the input file name here
+INPUT_FILE="rand.txt" #assign the input file name here
 OUTPUT_FILE="out.txt" #assign the output file name here
-Iterations=10 #assign number of iterations
+Iterations=1  #assign number of iterations
 
 arrData =  genfromtxt(INPUT_FILE, delimiter=" ")
 t=0 #initial state
@@ -26,6 +26,23 @@ arrDataX = np.array(arrData)
 slaveRankCount = size -1 #slaveRankCount assuming 16 as example
 slavesPerDimension = int(math.sqrt(slaveRankCount))#sqrt of slave num 4 as example
 lengthPerSlave = int(len(arrData)/slavesPerDimension)    #each slave's cell size
+
+sendSlice = [] #initialization the initial sent slice
+for i in range(lengthPerSlave):
+ column = []
+ for j in range(lengthPerSlave):
+  column.append(0)
+ sendSlice.append(column)
+sendSlice = np.array(sendSlice)
+
+mySliceX = [] #initialization the big array that includes the recieved top/up/right/left etc
+for i in range(lengthPerSlave+2):
+ column = []
+ for j in range(lengthPerSlave+2):
+  column.append(0)
+ mySliceX.append(column)
+mySliceX = np.array(mySliceX)
+
 def findTop(rank):#finding the top slave
     if rank<=slavesPerDimension:
         return slaveRankCount-(slavesPerDimension-rank)
@@ -52,7 +69,7 @@ def findRight(rank):    #finding the Right slave
 def gameOfLife(mySliceInput):   #game of life calculation
     for j in range(1,lengthPerSlave+1):#for rows
         for k in range(1,lengthPerSlave+1):#for columns
-            neighbor = mySliceInput[j-1][k-1]+mySliceInput[j-1][k]+mySliceInput[j-1][k+1]+mySliceInput[j][k-1]+mySliceInput[j][k+1]+mySliceInput[j+1][k-1]+mySliceInput[j+1][k]+mySliceInput[j+1][k+1]#calculating the sum of 1s in neighbors
+            neighbor = mySliceInput[j-1][k-1]+mySliceInput[j-1][k]+mySliceInput[j-1][k+1]+mySliceInput[j][k-1]+mySliceInput[j][k+1]+mySliceInput[j+1][k-1]+mySliceInput[j+1][k]+mySliceInput[j+1][k+1]  #calculating the sum of 1s in neighbors
             if mySliceInput[j][k]==0 and neighbor==3: #balance in all things
                 mySliceX[j][k]=1
             elif mySliceInput[j][k]==1 and neighbor<2:#loneliness also kills
@@ -65,21 +82,7 @@ def gameOfLife(mySliceInput):   #game of life calculation
     sendSlice=np.copy(mySliceX[1:lengthPerSlave+1,1:lengthPerSlave+1])
 
 
-sendSlice = [] #initialization the initial sent slice
-for i in range(lengthPerSlave):
- column = []
- for j in range(lengthPerSlave):
-  column.append(0)
- sendSlice.append(column)
-sendSlice = np.array(sendSlice)
 
-mySliceX = [] #initialization the big array that includes the recieved top/up/right/left etc
-for i in range(lengthPerSlave+2):
- column = []
- for j in range(lengthPerSlave+2):
-  column.append(0)
- mySliceX.append(column)
-mySliceX = np.array(mySliceX)
 
 
 if rank == 0:#for the manager 
@@ -111,7 +114,7 @@ else:
     temp = np.empty((lengthPerSlave+2,lengthPerSlave+2),dtype=int)
     temp = np.array(temp)
     temp.fill(0)    
-    len = len(sendSlice)
+    lenght = len(sendSlice)
     temp[1:lengthPerSlave+1,1:lengthPerSlave+1]=np.copy(sendSlice)
     row = (rank-1)//slavesPerDimension
     column = rank-(row*slavesPerDimension)-1
@@ -119,6 +122,7 @@ else:
     while(t<Iterations): # while for the iterations
         
         if rank%2==1:#For Odd Ones
+            sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findRight(findTop(rank)),tag=14)#RightTop sending to evens
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findRight(rank),tag=14)#Right
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findRight(findBot(rank)),tag=14)#RightBot
@@ -127,51 +131,54 @@ else:
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findLeft(findTop(rank)),tag=14)#LeftTop
 
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findRight(findTop(rank)),tag=14)#receive from right top even
-            temp[0,len+1]=sendSlice[0,lengthPerSlave-1]
+            temp[0,lenght+1]=sendSlice[lengthPerSlave-1,0]
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findRight(rank),tag=14)#receive from right
-            temp[1:len+1,len+1]=sendSlice[:,0]
+            temp[1:lenght+1,lenght+1]=sendSlice[:,0]
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(findRight(rank)),tag=14)#receive right bot
-            temp[len+1,len+1]=sendSlice[0,0]
+            temp[lenght+1,lenght+1]=sendSlice[0,0]
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(findLeft(rank)),tag=14)#receive bot left
-            temp[0,len+1]=sendSlice[0,lengthPerSlave-1]
+            temp[lenght+1,0]=sendSlice[0,lengthPerSlave-1]
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findLeft(rank),tag=14)# receive left
-            temp[0,1:len+1]=sendSlice[:,lengthPerSlave-1]
+            temp[1:lenght+1,0]=sendSlice[:,lengthPerSlave-1]
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(findLeft(rank)),tag=14)#receive top left
             temp[0,0]=sendSlice[lengthPerSlave-1,lengthPerSlave-1]
 
             if row%2==0:#Odd and Row even ie first row
+                sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findTop(rank),tag=14)#Top
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findBot(rank),tag=14)#Bot Sends
                 
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(rank),tag=14)#tag14 receive from Bot
-                temp[len+1,1:len+1]=np.copy(sendSlice[0,:])
+                temp[lenght+1,1:lenght+1]=np.copy(sendSlice[0,:])
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(rank),tag=14)#tag14 receive from Top
-                temp[0,1:len+1]=np.copy(sendSlice[lengthPerSlave-1,:])
+                temp[0,1:lenght+1]=np.copy(sendSlice[lengthPerSlave-1,:])
 
             else:#Odd and Row odd ie second row
                 
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(rank),tag=14)#tag14 receive from Bot
-                temp[len+1,1:len+1]=np.copy(sendSlice[0,:])
+                temp[lenght+1,1:lenght+1]=np.copy(sendSlice[0,:])
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(rank),tag=14)#tag14 receive from Top
-                temp[0,1:len+1]=np.copy(sendSlice[lengthPerSlave-1,:])
+                temp[0,1:lenght+1]=np.copy(sendSlice[lengthPerSlave-1,:])
 
+                sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findTop(rank),tag=14)#Top
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findBot(rank),tag=14)#Bot Sends
 
         else: #even
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(findLeft(rank)),tag=14)#Left Bot Receive from the odds
-            temp[len+1,0]=np.copy(sendSlice[0,lengthPerSlave-1])
+            temp[lenght+1,0]=np.copy(sendSlice[0,lengthPerSlave-1])
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findLeft(rank),tag=14)#Left Receive
-            temp[1:len+1,0]=np.copy(sendSlice[:,lengthPerSlave-1])
+            temp[1:lenght+1,0]=np.copy(sendSlice[:,lengthPerSlave-1])
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(findLeft(rank)),tag=14)#Left Top Receive
             temp[0,0]=np.copy(sendSlice[lengthPerSlave-1,lengthPerSlave-1])
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findRight(findTop(rank)),tag=14)#Rigt Top Receive
-            temp[0,len+1]=np.copy(sendSlice[0,lengthPerSlave-1])
+            temp[0,lenght+1]=np.copy(sendSlice[0,lengthPerSlave-1])
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findRight(rank),tag=14)#Right Receive
-            temp[1:len+1,len+1]=np.copy(sendSlice[:,0])
+            temp[1:lenght+1,lenght+1]=np.copy(sendSlice[:,0])
             COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(findRight(rank)),tag=14)#Right Bot Receive
-            temp[len+1,len+1]=np.copy(sendSlice[0,0])
+            temp[lenght+1,lenght+1]=np.copy(sendSlice[0,0])
             
+            sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findLeft(findBot(rank)),tag=14)#LeftBot sends to Odds
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findLeft(rank),tag=14)#Left
             COMM_WORLD.Send([sendSlice,MPI.INT],dest=findLeft(findTop(rank)),tag=14)#LeftTop
@@ -181,24 +188,26 @@ else:
             
             if row%2==0:#Even and even row ie first row
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(rank),tag=14)#tag14 receive from Bot
-                temp[len+1,1:len+1]=np.copy(sendSlice[0,:])
+                temp[lenght+1,1:lenght+1]=np.copy(sendSlice[0,:])
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(rank),tag=14)#tag14 receive from Top
-                temp[0,1:len+1]=np.copy(sendSlice[lengthPerSlave-1,:])
+                temp[0,1:lenght+1]=np.copy(sendSlice[lengthPerSlave-1,:])
 
+                sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findBot(rank),tag=14)#Top
                 COMM_WORLD.Send([sendSlice,MPI.INT],dest=findTop(rank),tag=14)#Bot Sends
 
                 
             else:#even and odd row ie second row
-                COMM_WORLD.Send([sendSlice,MPI.INT],dest=findTop(rank),tag=14)#Bot Sends                
-                COMM_WORLD.Send([sendSlice,MPI.INT],dest=findBot(rank),tag=14)#Top
+                sendSlice=np.copy(temp[1:lengthPerSlave+1,1:lengthPerSlave+1])
+                COMM_WORLD.Send([sendSlice,MPI.INT],dest=findTop(rank),tag=14)#Bot Sends to Top               
+                COMM_WORLD.Send([sendSlice,MPI.INT],dest=findBot(rank),tag=14)#Top sends to Bot
                 
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findTop(rank),tag=14)#tag14 receive from Top
-                temp[0,1:len+1]=np.copy(sendSlice[lengthPerSlave-1,:])
+                temp[0,1:lenght+1]=np.copy(sendSlice[lengthPerSlave-1,:])
                 COMM_WORLD.Recv([sendSlice,MPI.INT],source=findBot(rank),tag=14)#tag14 receive from Bot
-                temp[len+1,1:len+1]=np.copy(sendSlice[0,:])
-        
+                temp[lenght+1,1:lenght+1]=np.copy(sendSlice[0,:])
+        #print(temp,'is temp for rank',rank,'at iteration',t)
         gameOfLife(temp)
-        print(mySliceX,'is mySliceX for rank',rank,'at iteration',t+1)
+        #print(mySliceX,'is mySliceX for rank',rank,'at iteration',t+1)
         t+=1
     COMM_WORLD.Send([mySliceX,MPI.INT],dest=0,tag=14)#Return Back to Manager(rank=0)
